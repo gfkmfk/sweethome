338a296bc423d72365ad7f1ecf353d53435b38e5
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-02 19:01:03 +0300

    Update choose.sh

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -1,27 +1,18 @@
-#!/bin/bash
-echo
-echo "Please choose configution:"
-echo "a. Domoticz + Homebridge"
-echo "b. not yet realised"
-echo "c. not yet realised"
-read -p "a / b / c" RESP
-if [ "$RESP" = "a" ]; then
-echo "Choosed: a. Domoticz + Homebridge"
-# wget "https://github.com/gfkmfk/sweethome/raw/master/scripts/homebridge-install.sh" -O /usr/local/bin/homebridge-install.sh
-# chmod +x /usr/local/bin/homebridge-install.sh
-# /usr/local/bin/homebridge-install.sh
-/usr/local/bin/choose.sh
+#!/bin/sh
+
+promptyn () {
+    while true; do
+        read -p "$1 " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+if promptyn "is the sky blue?"; then
+    echo "yes"
 else
-  if [ "$RESP" = "b" ]; then
-  echo "b. not yet realised"
-  /usr/local/bin/choose.sh
-  else
-    if [ "$RESP" = "c" ]; then
-    echo "c. not yet realised"
-    /usr/local/bin/choose.sh
-    else
-      echo "Incorrect input!"
-      /usr/local/bin/choose.sh
-    fi
-  fi
+    echo "no"
 fi
