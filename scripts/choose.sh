2b65fb55f4bb442a3d85f03f0cfac04473982383
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-02 19:08:54 +0300

    Update choose.sh

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
-
-promptyn () {
-    while true; do
-        read -p "$1 " yn
-        case $yn in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-}
-
-if promptyn "is the sky blue?"; then
-    echo "yes"
-else
-    echo "no"
+echo
+echo "Please choose configution:"
+echo "a. Domoticz + Homebridge"
+echo "b. not yet realised"
+echo "c. not yet realised"
+echo "x. exit"
+read -p "Please choose: a/b/c/x" CONDITION;
+if [ "$CONDITION" == "a" ]; then
+   echo "Choosed: a. Domoticz + Homebridge"
+   # wget "https://github.com/gfkmfk/sweethome/raw/master/scripts/homebridge-install.sh" -O /usr/local/bin/homebridge-install.sh
+   # chmod +x /usr/local/bin/homebridge-install.sh
+   # /usr/local/bin/homebridge-install.sh
+   /usr/local/bin/choose.sh
+fi
+if [ "$CONDITION" == "b" ]; then
+   echo "Choosed: b. not yet realised"
+   /usr/local/bin/choose.sh
+fi
+if [ "$CONDITION" == "c" ]; then
+   echo "Choosed: c. not yet realised"
+   /usr/local/bin/choose.sh
+fi
+if [ "$CONDITION" == "x" ]; then
+   echo "Choosed: x. exit"
+   echo "Rebooting..."
+   reboot
 fi
