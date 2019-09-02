3916bf3f459ab98c906ade831b92a73fa71b6cd9
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-02 19:18:42 +0300

    Update choose.sh

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
-echo
-echo "Please choose configution:"
-echo "a. Domoticz + Homebridge"
-echo "b. not yet realised"
-read -p "Please choose: a/b/c/x" CONDITION;
-if [ "$CONDITION" == "a" ]; then
-   echo "Choosed: a. Domoticz + Homebridge"
-   # wget "https://github.com/gfkmfk/sweethome/raw/master/scripts/homebridge-install.sh" -O /usr/local/bin/homebridge-install.sh
-   # chmod +x /usr/local/bin/homebridge-install.sh
-   # /usr/local/bin/homebridge-install.sh
-   /usr/local/bin/choose.sh
-   else if [ "$CONDITION" == "b" ]; then
-     echo "Choosed: b. not yet realised"
-     /usr/local/bin/choose.sh
-     else
-       echo "Incorrect input!"
-       /usr/local/bin/choose.sh
-fi
+read -p "Is this a good question (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        echo "Yes"
+    ;;
+    n|N )
+        echo "No"
+    ;;
+    * )
+        echo "Incorrect input!"
+    ;;
+esac
