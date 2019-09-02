df6a2ff99bee993de8df4e9d64c8f421c4a662a1
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-02 18:54:47 +0300

    Update choose.sh

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -4,12 +4,24 @@ echo "Please choose configution:"
 echo "a. Domoticz + Homebridge"
 echo "b. not yet realised"
 echo "c. not yet realised"
-select yn in "a" "b" "c"; do
-    case $yn in
-        a ) wget "https://github.com/gfkmfk/sweethome/raw/master/scripts/homebridge-install.sh" -O /usr/local/bin/homebridge-install.sh; chmod +x /usr/local/bin/homebridge-install.sh; /usr/local/bin/homebridge-install.sh;;
-        b ) echo "nope"; CHOOSE;;
-        c ) echo "nope"; CHOOSE;;
-    esac
-done
-}
-CHOOSE
+read -p "a / b / c" RESP
+if [ "$RESP" = "a" ]; then
+echo "Choosed: a. Domoticz + Homebridge"
+# wget "https://github.com/gfkmfk/sweethome/raw/master/scripts/homebridge-install.sh" -O /usr/local/bin/homebridge-install.sh
+# chmod +x /usr/local/bin/homebridge-install.sh
+# /usr/local/bin/homebridge-install.sh
+/usr/local/bin/choose.sh
+else
+  if [ "$RESP" = "b" ]; then
+  echo "b. not yet realised"
+  /usr/local/bin/choose.sh
+  else
+    if [ "$RESP" = "c" ]; then
+    echo "c. not yet realised"
+    /usr/local/bin/choose.sh
+    else
+      echo "Incorrect input!
+      /usr/local/bin/choose.sh
+    fi
+  fi
+fi
