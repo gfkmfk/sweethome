71472e3027ee1457f2bbbad1416bf69f95e81fec
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-02 19:42:13 +0300

    Update choose.sh

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
+echo
+echo
+echo "Please choose configution:"
+echo
+echo "A. Domoticz + Homebridge"
+echo "B. Not yet realised"
+echo "C. Not yet realised"
+echo
+echo "X. Exit"
+echo
 while true
 do
   # (1) prompt user, and read command line argument
-  read -p "Run the cron script now? " answer
+  read -p "Please enter A, B, C or X: " answer
 
   # (2) handle the input we were given
   case $answer in
-   [aA]* ) echo "Okay, just ran the cron script.a"
+   [aA]* ) echo "Choosed: A. Domoticz + Homebridge"
+           # wget "https://github.com/gfkmfk/sweethome/raw/master/scripts/homebridge-install.sh" -O /usr/local/bin/homebridge-install.sh
+           # chmod +x /usr/local/bin/homebridge-install.sh
+           # /usr/local/bin/homebridge-install.sh
+           /usr/local/bin/choose.sh
            break;;
            
-   [bB]* ) echo "Okay, just ran the cron script.b"
+   [bB]* ) echo "Choosed: B. Not yet realised"
+           /usr/local/bin/choose.sh
            break;;
            
-   [cC]* ) echo "Okay, just ran the cron script.c"
+   [cC]* ) echo "Choosed: C. Not yet realised"
+           /usr/local/bin/choose.sh
            break;;
 
-   [xX]* ) exit;;
+   [xX]* ) "Exiting"
+           exit;;
 
    * )     echo "Dude, just enter A, B, C or X, please.";;
   esac
