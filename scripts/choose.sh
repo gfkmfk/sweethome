5094dfd0ec8c3c15dd67cc3506388938434f95b8
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-02 19:23:20 +0300

    Update choose.sh

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -3,17 +3,22 @@ read -p "Is this a good question (a/b/c/x)? " answer
 case ${answer:0} in
     a|A )
         echo "a"
+        /usr/local/bin/choose.sh
     ;;
     b|B )
         echo "b"
+        /usr/local/bin/choose.sh
     ;;
     c|C )
         echo "c"
+        /usr/local/bin/choose.sh
     ;;
     x|X )
         echo "x"
+        /usr/local/bin/choose.sh
     ;;
     * )
         echo "Incorrect input!"
+        /usr/local/bin/choose.sh
     ;;
 esac
