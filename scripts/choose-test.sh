fc14d61d3155bcac5817bd0a33097753ff5a1512
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-03 19:49:39 +0300

    Create choose-test.sh

---
+++ scripts/choose-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 
+while true
+do
+# Prompt user, and read command line argument
+read -p "Run the cron script now? " answer
+
+# Handle the command line argument we were given
+case $answer in
+   [yY]* ) echo "Okay, you choosed Y"
+           break;;
+
+   [nN]* ) echo "Great, you choosed N"
+           exit;;
+
+   * )     echo "Dude, just enter Y or N, please."; break ;;
+  esac
+done
