dbdaa366e8d0acbb03b6d96ff886f9f6c69a20d8
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-03 23:54:11 +0300

    Update choose.sh

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
+option_a {
+echo ""
+echo "Choosed - A. Domoticz + Homebridge"
+echo "Running script"
+echo ""
+curl -L https://github.com/gfkmfk/sweethome/raw/master/scripts/domoticz-install.sh -o /usr/local/bin/domoticz-install.sh
+chmod +x /usr/local/bin/domoticz-install.sh
+/usr/local/bin/domoticz-install.sh
+exit 1
+}
+option_b {
+echo ""
+echo "Choosed - B. Ubiquity WiFi controller"
+echo "Running script"
+echo ""
+curl -L https://github.com/gfkmfk/sweethome/raw/master/scripts/wificontroller-install.sh -o /usr/local/bin/wificontroller-install.sh
+chmod +x /usr/local/bin/wificontroller-install.sh
+/usr/local/bin/wificontroller-install.sh
+exit 1
+}
+option_c {
+echo ""
+echo "Choosed - C. Not yet realised"
+echo "Calling ask_func"
+echo ""
+ask_func
+}
+option_d {
+echo ""
+echo "Choosed - D. Not yet realised"
+echo "Calling ask_func"
+echo ""
+ask_func
+}
+option_x {
+echo ""
+echo "Okay, exiting..."
+exit 1
+}
+option_f {
+echo ""
+echo "Dude, choose A, B, C, D or X 🤬"
+echo ""
+ask_func
+}
+welcome_func {
+clear
 echo ""
 echo "Please choose configution:"
 echo ""
@@ -9,44 +56,20 @@ echo "D. Not yet realised"
 echo ""
 echo "X. Exit"
 echo ""
-while true
-do
-  # (1) prompt user, and read command line argument
-  read -p "Please enter A, B, C, D or X: " answer
-
-  # (2) handle the input we were given
-  case $answer in
-   [aA]* ) echo ""
-           echo "Choosed: A. Domoticz + Homebridge"
-           echo ""
-           echo "Running sript"
-           echo ""
-           exec curl -L https://github.com/gfkmfk/sweethome/raw/master/scripts/homebridge-install.sh | sudo bash
-           break;;
-           
-   [bB]* ) echo ""
-           echo "Choosed: B. Ubiquity WiFi controller"
-           echo ""
-           echo "Running sript"
-           echo ""
-           exec curl -L https://github.com/gfkmfk/sweethome/raw/master/scripts/wificontroller-install.sh | sudo bash
-           break;;
-           
-   [cC]* ) echo ""
-           echo "Choosed: C. Not yet realised"
-           echo "";;
-           
-   [dD]* ) echo ""
-           echo "Choosed: D. Not yet realised"
-           echo "";;
-
-   [xX]* ) echo ""
-           echo "Exiting"
-           echo ""
-           exit;;
-
-   * )     echo ""
-           echo "Dude, just enter A, B, C, D or X, please."
-           echo "";;
-  esac
-done
+ask_func
+}
+ask_func {
+  while true
+    do
+      read -p "Please enter A, B, C, D or X: " answer
+      case $answer in
+        [aA]* ) option_a;;
+        [bB]* ) option_b;;
+        [cC]* ) option_c;;
+        [dD]* ) option_d;;
+        [xX]* ) option_x;;
+        * )     option_f;;
+    esac
+  done
+}
+welcome_func
