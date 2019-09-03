af5e9e198b4d3f5f49985dabe0577c801fc17c32
gfkmfk <54787724+gfkmfk@users.noreply.github.com>
2019-09-03 20:24:44 +0300

    Update choose-test.sh

--- scripts/choose-test.sh
+++ scripts/choose-test.sh
@@ -1,15 +1,69 @@
 #!/bin/bash
-# Prompt user, and read command line argument
-read -p "Run the cron script now? " answer
 
-# Handle the command line argument we were given
+# Choose options
+choose_1="1. Homebridge + Domoticz"
+choose_2="2. UniFi Controller"
+choose_3=""
+choose_4=""
+
+# Option's functions
+option_1() {
+echo "test opt_1"
+}
+option_2() {
+echo "test opt_2"
+}
+option_3() {
+echo "test opt_3"
+}
+option_4() {
+echo "test opt_4"
+}
+
+# Welcome function
+welcome_sessage() {
+  echo 
+  echo "     Hello and Welcome!"
+  echo 
+  echo "Choose one of the following"
+  echo "configuration  to  install:"
+  echo 
+  echo "${choose_1}"
+  echo "${choose_2}"
+  echo "${choose_3}"
+  echo "${choose_4}"
+  echo 
+  ask_script
+}
+
+# Ask function
+ask_script() {
+while true
+do
+read -p "What you choose?: " answer
 case $answer in
-   [yY]* ) echo "Okay, you choosed Y"
+   [1]* ) echo "Okay, you choosed ${choose_1}"
+          sleep 3
+          choose_1
            break;;
 
-   [nN]* ) echo "Great, you choosed N"
-           exit;;
+   [2]* ) echo "Okay, you choosed ${choose_2}"
+          sleep 3
+          choose_2
+           break;;
 
-   * )     echo "Dude, just enter Y or N, please."; break ;;
+   [3]* ) echo "Okay, you choosed ${choose_3}"
+          sleep 3
+          choose_3
+           break;;
+
+   [4]* ) echo "Okay, you choosed ${choose_4}"
+          sleep 3
+          choose_4
+           break;;
+
+   * )     echo "Dude, just enter 1, 2, 3 or 4, please.";;
   esac
 done
+}
+main "$welcome_message"
